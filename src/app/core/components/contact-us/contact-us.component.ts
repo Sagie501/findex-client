32801b339d3cca9881892587fb31a3d4148ddb7c
tomshudev <tomshudev@gmail.com>
2020-07-18 20:17:21 +0300

    Display pin for every registered user

--- src/app/core/components/contact-us/contact-us.component.ts
+++ src/app/core/components/contact-us/contact-us.component.ts
@@ -1,33 +1,41 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { UserService } from '../../services/user/user.service';
 declare var Microsoft;
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.less']
+  styleUrls: ['./contact-us.component.less'],
 })
 export class ContactUsComponent implements OnInit, AfterViewInit {
-
   @ViewChild('myMap') myMap;
 
-  constructor() { }
+  constructor(private usersService: UserService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    const  map = new Microsoft.Maps.Map(this.myMap.nativeElement, {
-      credentials: 'Bing Map Key - I removed it here'
+    const map = new Microsoft.Maps.Map(this.myMap.nativeElement, {
+      credentials: 'Bing Map Key - I removed it here',
     });
 
     map.setView({
       center: new Microsoft.Maps.Location(31.970575, 34.767812),
-      zoom: 15
+      zoom: 15,
     });
 
     let pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
     let layer = new Microsoft.Maps.Layer();
     layer.add(pushpin);
     map.layers.insert(layer);
+
+    this.usersService.getAllUsersLocations().subscribe((coordinates) => {
+      let userLocation = new Microsoft.Maps.Location(
+        coordinates[0],
+        coordinates[1]
+      );
+      let userPin = new Microsoft.Maps.Pushpin(userLocation, null);
+      layer.add(userPin);
+    });
   }
 }
